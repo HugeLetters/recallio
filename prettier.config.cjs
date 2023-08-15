91d5449b7d153bfddaabf7273c7079a32e4cb7e4
Huge Letters <ckuka71@gmail.com>
2023-08-15 10:11:22 +0200

    basic camera access

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  printWidth: 100,
+  singleAttributePerLine: true,
 };
 
 module.exports = config;
