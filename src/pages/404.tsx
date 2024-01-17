252fc2032875d4341a7781513aab5bd3d28b8400
hugeletters <ckuka71@gmail.com>
2024-01-17 03:20:32 +0100

    FUCK OFF

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,1 @@
-import { Error } from "@/components/Error";
-
-export default function Page() {
-  return <Error errorMessage="Page not found" />;
-}
+import { Error } from "@/components/Error";export default function Page() {  return <Error errorMessage="Page not found" />;}
