e07f5fdfcd8f6413dab78203887e8b1ad1ff015f
hugeletters <ckuka71@gmail.com>
2024-03-15 22:19:29 +0100

    remove comments

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,9 +1,3 @@
-/**
- * This is the client-side entrypoint for your tRPC API. It is used to create the `api` object which
- * contains the Next.js App-wrapper, as well as your type-safe React Query hooks.
- *
- * We also create a few inference helpers for input and output types.
- */
 import { signOut } from "@/auth";
 import { browser } from "@/browser";
 import { toast } from "@/components/toast";
@@ -13,8 +7,6 @@ import { QueryCache } from "@tanstack/react-query";
 import { TRPCClientError, httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
-
-// todo - https://responsively.app/
 
 function getBaseUrl() {
   if (browser) return ""; // browser should use relative url
