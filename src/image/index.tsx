ea3a47211c9f5ece5bbbb1d4ac13c313a1fe3a4b
hugeletters <ckuka71@gmail.com>
2024-03-19 15:22:52 +0100

    image with fallback first draft

---
+++ src/image/index.tsx
@@ -0,0 +1,30 @@
+import type { ExtendPropety } from "@/utils/object";
+import { hasTruthyProperty } from "@/utils/object";
+import NextImage from "next/image";
+import type { ComponentPropsWithoutRef } from "react";
+import { useState } from "react";
+
+type NextImageProps = ComponentPropsWithoutRef<typeof NextImage>;
+type ImageProps = ExtendPropety<NextImageProps, "src", null | undefined>;
+/**
+ * `children` props acts as a fallback when image couldn't be loaded
+ */
+export function Image({ children, onLoad, onError, ...props }: ImageProps) {
+  const [forceFallback, setForceFallback] = useState(false);
+
+  return hasTruthyProperty(props, "src") && !forceFallback ? (
+    <NextImage
+      onLoad={(e) => {
+        setForceFallback(false);
+        onLoad?.(e);
+      }}
+      onError={(e) => {
+        setForceFallback(true);
+        onError?.(e);
+      }}
+      {...props}
+    />
+  ) : (
+    children
+  );
+}
