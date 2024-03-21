f27b467913bea57c62c053260d9d6570fe310cde
hugeletters <ckuka71@gmail.com>
2024-03-21 01:18:57 +0100

    set cookie helper

--- src/encode/cookie.ts
+++ src/encode/cookie.ts
@@ -2,6 +2,7 @@ import { browser } from "@/browser";
 import { mapFilter } from "@/utils/array/filter";
 import type { Option } from "@/utils/option";
 import { isSome } from "@/utils/option";
+import type { Nullish } from "@/utils/type";
 import { decodeJSON } from "./json";
 
 export function getCookies(): Option<Record<string, unknown>> {
@@ -22,4 +23,24 @@ export function getCookies(): Option<Record<string, unknown>> {
   );
 
   return { ok: true, value: Object.fromEntries(cookieEntries) };
+}
+
+type Cookie = {
+  name: string;
+  value: string;
+  path?: string;
+  expiry?: Date;
+  secure?: boolean;
+  sameSite?: string;
+};
+export function setCookie({ name, value, expiry, path, sameSite, secure }: Cookie) {
+  if (!browser) return;
+  const cookie = `${name}=${value}; ${cookieChunk("Expires", expiry?.toUTCString())} ${cookieChunk("Path", path)} ${cookieChunk("sameSite", sameSite)} ${cookieChunk("Secure", secure)}`;
+  document.cookie = cookie;
+}
+
+function cookieChunk(name: string, value: Nullish<string | boolean>): string {
+  if (!value) return "";
+  if (typeof value === "boolean") return `${name};`;
+  return `${name}=${value};`;
 }
