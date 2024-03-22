02a218158ad470616e055acf9b0849452b3d4d1c
hugeletters <ckuka71@gmail.com>
2024-03-22 14:18:50 +0100

    todos

--- src/auth/guard.tsx
+++ src/auth/guard.tsx
@@ -3,16 +3,16 @@ import { setSessionDataCookie } from "@/auth/session-cookie";
 import type { ReactNode } from "react";
 import { useEffect } from "react";
 
+// todo - how should this be structured?
+// ? Should session data always be set in all routes?
+// ? Should this guard block render? I don't think so
+// ? Do I even need it? Maybe just use required session where I need to?
 export function AuthGuard({ children }: { children: ReactNode }) {
   const { data } = useRequiredSession();
   useEffect(() => {
     if (!data) return;
     setSessionDataCookie(data);
   }, [data]);
-
-  if (!data) {
-    return <div className="flex size-full items-center justify-center text-8xl">Loading...</div>;
-  }
 
   return children;
 }
