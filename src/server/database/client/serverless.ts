d117c2192983321f7813ffdbec827d66a3dca5be
hugeletters <ckuka71@gmail.com>
2024-03-22 14:12:14 +0100

    todo

--- src/server/database/client/serverless.ts
+++ src/server/database/client/serverless.ts
@@ -4,5 +4,6 @@ import { createClient } from "libsql-stateless-easy";
 
 const connection = createClient({ url: env.DATABASE_HTTPS_URL, authToken: env.DATABASE_TOKEN });
 
+// todo - use it where possible
 export const db = drizzle(connection);
 export type DatabaseClient = typeof db;
