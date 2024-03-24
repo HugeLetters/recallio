c86018fd9f0a76f5dedb5e56bf93575e6890025e
hugeletters <ckuka71@gmail.com>
2024-03-24 13:39:21 +0100

    set difference util

--- src/utils/set.ts
+++ src/utils/set.ts
@@ -1,9 +1,21 @@
 export function setIntersection<T>(a: Set<T>, b: Set<T>) {
   const intersection = new Set<T>();
   for (const value of a) {
-    if (b.has(value)) intersection.add(value);
+    if (b.has(value)) {
+      intersection.add(value);
+    }
   }
   return intersection;
+}
+
+export function setDifference<T>(a: Set<T>, b: Set<T>) {
+  const difference = new Set<T>();
+  for (const value of a) {
+    if (!b.has(value)) {
+      difference.add(value);
+    }
+  }
+  return difference;
 }
 
 export function isSetEqual<T>(a: Set<T>, b: Set<T>) {
