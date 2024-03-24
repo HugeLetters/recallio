b678669e9f813c80e1cd0603bf5b96752a8e813a
hugeletters <ckuka71@gmail.com>
2024-03-24 02:49:49 +0100

    minor refactor

--- src/router/check.ts
+++ src/router/check.ts
@@ -1,20 +1,14 @@
 import type { RouteType } from "./matcher";
 import { dynamic, routeEnd, routeMatcher } from "./matcher";
 
-type MatcherNode = typeof routeMatcher;
 export function getRouteInfo(pathname: string): RouteType {
   const chunks = pathname.split("/");
   let node = routeMatcher;
   for (const chunk of chunks) {
     if (!chunk) continue;
-    const nextNode = getNextNode(node, chunk);
+    const nextNode = node[chunk in node ? chunk : dynamic];
     if (!nextNode) return "public";
     node = nextNode;
   }
   return node[routeEnd] ?? "public";
-}
-
-function getNextNode(node: MatcherNode, chunk: string) {
-  if (chunk in node) return node[chunk];
-  return node[dynamic];
 }
