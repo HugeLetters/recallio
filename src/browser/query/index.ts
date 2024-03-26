e7aaae8d08ff579955a44ae2f5f9351fbf671c49
hugeletters <ckuka71@gmail.com>
2024-03-26 21:59:10 +0100

    fix formatting

--- src/browser/query/index.ts
+++ src/browser/query/index.ts
@@ -27,5 +27,3 @@ export function setQueryParam({ key, router, push, value }: SetQueryParamOptions
     router.replace({ query: router.query }, undefined, { shallow: true }).catch(console.error);
   }
 }
-
-
