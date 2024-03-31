6566a31113dee9815aa34ee21596f732af6405c1
hugeletters <ckuka71@gmail.com>
2024-04-01 00:39:47 +0200

    create arrow button component

--- src/browser/scroll-up/index.tsx
+++ src/browser/scroll-up/index.tsx
@@ -1,11 +1,11 @@
 import { Transition } from "@/animation/transition";
+import { ArrowButton } from "@/components/ui/arrow-button";
 import { scrollUpButtonEnabledStore } from "@/settings/boolean";
 import { useStore } from "@/state/store";
 import type { TrackerStore } from "@/state/store/tracker";
 import { tw } from "@/styles/tw";
 import type { FC } from "react";
 import React, { useEffect, useRef, useState } from "react";
-import RightIcon from "~icons/formkit/right";
 
 type ScrollUpProps = {
   threshold?: number;
@@ -54,7 +54,7 @@ export const ScrollUpButton = ScrollUpButtonEnabledGuard(function ({
         {show && isThershold && (
           <div className={tw("fixed z-0", className)}>
             <div className="-translate-x-full -translate-y-full">
-              <button
+              <ArrowButton
                 type="button"
                 aria-label="Scroll to top of the section"
                 onClick={() => {
@@ -62,10 +62,7 @@ export const ScrollUpButton = ScrollUpButtonEnabledGuard(function ({
                   if (!container) return;
                   container.scrollTo({ top: 0, behavior: "smooth" });
                 }}
-                className="group clickable flex aspect-square items-center rounded-full bg-app-green-500 p-1 text-white ring-1 ring-white shadow-around sa-o-25 sa-r-1"
-              >
-                <RightIcon className="size-8 -rotate-90 transition-transform duration-200 group-active:-translate-y-0.5" />
-              </button>
+              />
             </div>
           </div>
         )}
---
+++ src/components/ui/arrow-button.tsx
@@ -0,0 +1,19 @@
+import { tw } from "@/styles/tw";
+import type { StrictOmit } from "@/utils/object";
+import type { ComponentPropsWithoutRef } from "react";
+import RightIcon from "~icons/formkit/right";
+
+type ArrowButtonProps = StrictOmit<ComponentPropsWithoutRef<"button">, "children">;
+export function ArrowButton({ className, ...props }: ArrowButtonProps) {
+  return (
+    <button
+      {...props}
+      className={tw(
+        "group clickable primary flex aspect-square items-center rounded-full p-1 ring-1 ring-white shadow-around sa-o-25 sa-r-1",
+        className,
+      )}
+    >
+      <RightIcon className="size-8 -rotate-90 transition-transform duration-200 group-active:-translate-y-0.5" />
+    </button>
+  );
+}
