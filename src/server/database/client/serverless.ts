f94f5f3d8c06ef7634b30b6bb60e6993d2d87be6
hugeletters <ckuka71@gmail.com>
2024-04-06 01:17:32 +0200

    add local db support

--- src/server/database/client/serverless.ts
+++ src/server/database/client/serverless.ts
@@ -1,7 +1,10 @@
 import { env } from "@/server/env/index.mjs";
+import { isDev } from "@/utils";
+import { createClient as createStatefulClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "libsql-stateless-easy";
+import { createClient as createServerlessClient } from "libsql-stateless-easy";
 
+const createClient = !isDev ? createServerlessClient : createStatefulClient;
 const connection = createClient({ url: env.DATABASE_HTTPS_URL, authToken: env.DATABASE_TOKEN });
 
 export const db = drizzle(connection);
