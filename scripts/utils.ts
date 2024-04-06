4a7b58b3ef9f6463519b15774ec0baff6cb5e640
hugeletters <ckuka71@gmail.com>
2024-04-06 19:15:45 +0200

    log stdout even if there was stderr when executing process

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -4,6 +4,7 @@ import { promisify } from "node:util";
 export function execAsync(command: string) {
   return promisify(exec)(command).then(({ stderr, stdout }) => {
     if (stderr) {
+      console.log(stdout);
       throw Error(stderr);
     }
     return stdout;
