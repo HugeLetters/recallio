ed2c015147fd7fc3614dd833a318ec1d9d1f864a
hugeletters <ckuka71@gmail.com>
2024-04-08 00:11:55 +0200

    fix execAsync util - it throws on process errors

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -4,8 +4,7 @@ import { promisify } from "node:util";
 export function execAsync(command: string) {
   return promisify(exec)(command).then(({ stderr, stdout }) => {
     if (stderr) {
-      console.log(stdout);
-      throw Error(stderr);
+      console.error(stderr);
     }
     return stdout;
   });
