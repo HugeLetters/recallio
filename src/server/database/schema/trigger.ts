e6dc13d4d11e4115ff9c39f416f7067e7a6da093
hugeletters <ckuka71@gmail.com>
2024-04-07 16:40:45 +0200

    trigger creation util done

--- src/server/database/schema/trigger.ts
+++ src/server/database/schema/trigger.ts
@@ -1,100 +1,121 @@
-import type { NonEmptyArray } from "@/utils/array";
 import type { StrictExclude, StrictExtract } from "@/utils/type";
-import type { Query, SQLWrapper } from "drizzle-orm";
-import { eq, ne, sql } from "drizzle-orm";
-import type { SQLiteTableWithColumns, TableConfig } from "drizzle-orm/sqlite-core";
+import type { Query, SQL, SQLWrapper } from "drizzle-orm";
+import { sql } from "drizzle-orm";
+import type { BuildAliasTable, SQLiteTable } from "drizzle-orm/sqlite-core";
 import { SQLiteSyncDialect, alias } from "drizzle-orm/sqlite-core";
-import { caseWhen, space } from "./utils";
+import { space } from "./utils";
 
-type TableConfigColumn<TConfig extends TableConfig> = TConfig["columns"][keyof TConfig["columns"]];
+type TableColumn<TTable extends SQLiteTable> = TTable["_"]["columns"][keyof TTable["_"]["columns"]];
 
 type TriggerType = "INSERT" | "UPDATE" | "DELETE";
-type TriggerRowData<TType extends TriggerType, TConfig extends TableConfig> = {
-  [K in
-    | (TType extends StrictExclude<TriggerType, "DELETE"> ? "newRow" : never)
-    | (TType extends StrictExclude<TriggerType, "INSERT"> ? "oldRow" : never)]: TConfig["columns"];
-};
+type TriggerRow<TType extends TriggerType, TTable extends SQLiteTable> = Record<
+  TType extends StrictExclude<TriggerType, "DELETE"> ? "newRow" : never,
+  BuildAliasTable<TTable, "new">
+> &
+  Record<
+    TType extends StrictExclude<TriggerType, "INSERT"> ? "oldRow" : never,
+    BuildAliasTable<TTable, "old">
+  >;
 
-type BaseTriggerData<TType extends TriggerType, TConfig extends TableConfig> = {
+type BaseTriggerData<TType extends TriggerType, TTable extends SQLiteTable> = {
   /** Trigger name */
   name: string;
   /** On which operations should trigger activate */
   type: TType;
   /** On which tables should trigger activate */
-  on: SQLiteTableWithColumns<TConfig>;
+  on: TTable;
   /** Condition when trigger should activate */
-  when?: (row: TriggerRowData<TType, TConfig>) => SQLWrapper;
+  when?: (row: TriggerRow<TType, TTable>) => SQLWrapper;
   /** WHat operation to perform when trigger activates */
-  do: (row: TriggerRowData<TType, TConfig>) => SQLWrapper;
+  do: (row: TriggerRow<TType, TTable>) => SQLWrapper;
 };
 
-interface UpdateTriggerData<TConfig extends TableConfig>
-  extends BaseTriggerData<"UPDATE", TConfig> {
+interface UpdateTriggerData<TTable extends SQLiteTable> extends BaseTriggerData<"UPDATE", TTable> {
   /** On which column of `on` table should trigger activate */
-  of?: TableConfigColumn<TConfig>;
+  of?: TableColumn<TTable>;
 }
 
 type TriggerData<
   TType extends TriggerType = TriggerType,
-  TConfig extends TableConfig = TableConfig,
+  TTable extends SQLiteTable = SQLiteTable,
 > =
   TType extends StrictExtract<TriggerType, "UPDATE">
-    ? UpdateTriggerData<TConfig>
-    : BaseTriggerData<TType, TConfig>;
+    ? UpdateTriggerData<TTable>
+    : BaseTriggerData<TType, TTable>;
 
 const sqlite = new SQLiteSyncDialect();
 const endLine = sql`;`;
-const breakpoint = sql`--> statement-breakpoint\n`;
+const newLine = sql`\n`;
+const tab = sql`\t`;
+const breakpoint = sql`--> statement-breakpoint`.append(newLine);
 
-// todo - builder pattern!
+// todo - script to generate trigger migrations
+// #1 - scan all migration files for trigger creations and then drop all of them
+// #2 - regenerate all triggers
 
-export function createTrigger<TType extends TriggerType, TConfig extends TableConfig>(
-  data: TriggerData<TType, TConfig>,
-) {
-  return sql.join(
-    [createDropTriggerStatement(data.name), createTriggerStatement(data)],
-    breakpoint,
-  );
-}
+class Trigger<TType extends TriggerType = TriggerType, TTable extends SQLiteTable = SQLiteTable> {
+  statement: SQL;
 
-export function createMigration(...queryList: NonEmptyArray<SQLWrapper>) {
-  return sqlite.sqlToQuery(sql.join(queryList, breakpoint));
-}
+  constructor(data: TriggerData<TType, TTable>) {
+    const dropStatement = join(
+      [sql`DROP TRIGGER IF EXISTS`, sql.identifier(data.name)],
+      space,
+    ).append(endLine);
 
-function createDropTriggerStatement(name: string) {
-  return sql.join([sql`DROP TRIGGER IF EXISTS`, sql.identifier(name)], space).append(endLine);
-}
-
-function createTriggerStatement(data: TriggerData) {
-  const newTable = alias(data.on, "new");
-  const oldTable = alias(data.on, "old");
-
-  return sql
-    .join(
+    const triggerTable = join(
       [
-        sql`CREATE TRIGGER`,
-        sql.identifier(data.name),
         sql`AFTER`,
         sql.raw(data.type),
         data.type === "UPDATE" && data.of
-          ? sql.join([sql`OF`, sql.identifier(data.of.name)], space)
+          ? join([sql`OF`, sql.identifier(data.of.name)], space)
           : undefined,
         sql`ON`,
         data.on,
-        data.when
-          ? sql.join(
-              [sql`FOR EACH ROW WHEN`, data.when({ newRow: newTable, oldRow: oldTable }).getSQL()],
-              space,
-            )
-          : undefined,
-        sql`BEGIN`,
-        data.do({ newRow: newTable, oldRow: oldTable }).getSQL(),
-        endLine,
-        sql`END`,
-      ].filter(Boolean),
+      ],
       space,
-    )
-    .append(endLine);
+    );
+
+    const newTable = alias(data.on, "new");
+    const oldTable = alias(data.on, "old");
+    const triggerCondition = data.when
+      ? join([
+          tab,
+          join(
+            [sql`FOR EACH ROW WHEN`, data.when({ newRow: newTable, oldRow: oldTable }).getSQL()],
+            space,
+          ),
+        ])
+      : undefined;
+
+    const createStatement = join(
+      [
+        join([sql`CREATE TRIGGER`, sql.identifier(data.name)], space),
+        join([tab, triggerTable]),
+        triggerCondition,
+        sql`BEGIN`,
+        join([tab, data.do({ newRow: newTable, oldRow: oldTable }).getSQL(), endLine]),
+        sql`END`,
+      ],
+      newLine,
+    ).append(endLine);
+
+    this.statement = join([dropStatement, createStatement], breakpoint);
+  }
+}
+
+export function createTrigger<TType extends TriggerType, TTable extends SQLiteTable>(
+  data: TriggerData<TType, TTable>,
+) {
+  return new Trigger(data);
+}
+
+function createMigration(...queryList: Array<Trigger>) {
+  return sqlite.sqlToQuery(
+    join(
+      queryList.map((trigger) => trigger.statement),
+      breakpoint,
+    ),
+  );
 }
 
 function serializeQuery(query: Query) {
@@ -112,3 +133,6 @@ function serializeQuery(query: Query) {
     .join("")}`;
 }
 
+const join: typeof sql.join = function (chunks, separator) {
+  return sql.join(chunks.filter(Boolean), separator);
+};
