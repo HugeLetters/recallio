7a5a2a014a6aaec4d30d58c3d1e9563a679a0d76
hugeletters <ckuka71@gmail.com>
2024-04-08 14:06:49 +0200

    delete review query upd

--- src/server/api/user/review/delete.ts
+++ src/server/api/user/review/delete.ts
@@ -4,7 +4,7 @@ import { nonNullableSQL } from "@/server/database/query";
 import { review } from "@/server/database/schema/product";
 import { throwExpectedError } from "@/server/error/trpc";
 import { createBarcodeSchema } from "@/server/product/validation";
-import { createFileDeleteQueueQuery } from "@/server/uploadthing/delete-queue";
+import { fileDeleteQueueQuery } from "@/server/uploadthing/delete-queue";
 import { ignore } from "@/utils";
 import { and, eq, isNotNull } from "drizzle-orm";
 import { z } from "zod";
@@ -19,11 +19,13 @@ export const deleteReview = protectedProcedure
       .select({ fileKey: nonNullableSQL(review.imageKey) })
       .from(review)
       .where(and(filter, isNotNull(review.imageKey)))
-      .then((files) => {
-        return db.batch([
-          db.delete(review).where(filter),
-          ...createFileDeleteQueueQuery(db, files),
-        ]);
+      .then<unknown>((files) => {
+        const deleteReview = db.delete(review).where(filter);
+        const deleteFiles = fileDeleteQueueQuery(db, files);
+
+        if (!deleteFiles) return deleteReview;
+
+        return db.batch([deleteReview, deleteFiles]);
       })
       .then(ignore)
       .catch(throwExpectedError(`Failed to delete your review for barcode ${barcode}.`));
