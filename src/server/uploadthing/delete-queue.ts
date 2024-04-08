184c41a5ea6dd6d3a315b7ae7ab24500389a8e28
hugeletters <ckuka71@gmail.com>
2024-04-08 14:06:29 +0200

    simplify file delete queue query util

--- src/server/uploadthing/delete-queue.ts
+++ src/server/uploadthing/delete-queue.ts
@@ -2,7 +2,7 @@ import type { DatabaseClient } from "@/server/database/client";
 import { fileDeleteQueue } from "@/server/database/schema/file";
 
 type PendingFile = typeof fileDeleteQueue.$inferInsert;
-export function createFileDeleteQueueQuery(client: DatabaseClient, files: Array<PendingFile>) {
-  if (!files.length) return [] satisfies [];
-  return [client.insert(fileDeleteQueue).values(files).onConflictDoNothing()] satisfies [unknown];
+export function fileDeleteQueueQuery(client: DatabaseClient, files: Array<PendingFile>) {
+  if (!files.length) return null;
+  return client.insert(fileDeleteQueue).values(files).onConflictDoNothing();
 }
