0b846fc7638918b4d6c7cb2a872f43d3a18c2165
hugeletters <ckuka71@gmail.com>
2024-05-01 20:39:06 +0200

    update local storage settings across tabs

--- src/settings/boolean.ts
+++ src/settings/boolean.ts
@@ -1,25 +1,18 @@
 import { browser } from "@/browser";
 import { Store } from "@/state/store";
 
-// todo - track other tabs
 class BooleanSettingStore extends Store<boolean> {
-  private static initialize(key: string, initial: boolean): boolean {
-    if (!browser) return initial;
-
-    const localValue = localStorage.getItem(key);
-    if (localValue === null) {
-      localStorage.setItem(key, `${initial}`);
-      return initial;
-    }
-
-    return localValue !== "false";
-  }
-
   constructor(
     private key: string,
     private initial: boolean,
   ) {
-    super(BooleanSettingStore.initialize(key, initial));
+    super(initializeStore(key, initial));
+    if (!browser) return;
+
+    window.addEventListener("storage", (ev) => {
+      if (ev.key !== key) return;
+      this.setState(ev.newValue !== null ? stringToBoolean(ev.newValue) : initial);
+    });
   }
 
   setValue = (value: boolean) => {
@@ -27,6 +20,22 @@ class BooleanSettingStore extends Store<boolean> {
     localStorage.setItem(this.key, `${value}`);
   };
   getServerSnapshot = () => this.initial;
+}
+
+function initializeStore(key: string, initial: boolean): boolean {
+  if (!browser) return initial;
+
+  const localValue = localStorage.getItem(key);
+  if (localValue === null) {
+    localStorage.setItem(key, `${initial}`);
+    return initial;
+  }
+
+  return stringToBoolean(localValue);
+}
+
+function stringToBoolean(value: string) {
+  return value !== "false";
 }
 
 export const reviewPrivateDefaultStore = new BooleanSettingStore("review-private-default", true);
