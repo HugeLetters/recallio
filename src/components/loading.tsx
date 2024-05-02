132081a5387e6a7466614a8c405eeaba7513c3f9
hugeletters <ckuka71@gmail.com>
2024-05-03 00:12:57 +0200

    add blur to skeleton

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -7,8 +7,8 @@ const error = style.error!;
 export function Skeleton() {
   return (
     <div className="flex size-full flex-col gap-1">
-      <div className={tw("size-full rounded-xl", root)} />
-      <div className={tw("size-full rounded-xl", root, error)} />
+      <div className={tw("size-full rounded-xl blur-sm", root)} />
+      <div className={tw("size-full rounded-xl blur-sm", root, error)} />
     </div>
   );
 }
