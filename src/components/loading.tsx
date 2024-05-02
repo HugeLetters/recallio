ca4a7d1d2ada03e78d2fb85175563d6b7bcd342e
hugeletters <ckuka71@gmail.com>
2024-05-02 23:38:10 +0200

    add error skeleton

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -2,7 +2,13 @@ import { tw } from "@/styles/tw";
 import style from "./loading.module.scss";
 
 const root = style.root!;
+const error = style.error!;
 
 export function Skeleton() {
-  return <div className={tw("size-full rounded-xl", root)} />;
+  return (
+    <div className="flex size-full flex-col gap-1">
+      <div className={tw("size-full rounded-xl", root)} />
+      <div className={tw("size-full rounded-xl", root, error)} />
+    </div>
+  );
 }
