0cfa6e176ed09114de7cb32ce31bfd6ccae833a5
hugeletters <ckuka71@gmail.com>
2024-05-03 14:59:57 +0200

    add module for dev-only utils

---
+++ src/utils/dev.ts
@@ -0,0 +1,35 @@
+/**
+ * Some function which may be useful during development
+ * @module utils/dev
+ */
+import { env } from "@/server/env/index.mjs";
+import { useEffect, useState } from "react";
+
+if (env.NEXT_PUBLIC_NODE_ENV !== "development") {
+  throw Error("Do not use this module outside of development");
+}
+
+export function useDelayed(delay: number) {
+  const [value, setValue] = useState(false);
+
+  useEffect(() => {
+    const t = setTimeout(() => {
+      setValue(true);
+    }, delay);
+
+    return () => {
+      clearTimeout(t);
+    };
+  }, [delay]);
+
+  return value;
+}
+
+export function createTimer() {
+  let time = performance.now();
+  return function (label?: string) {
+    const t = performance.now();
+    console.log(`${label ?? "TIMER"}: ${t - time}`);
+    time = t;
+  };
+}
