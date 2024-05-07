9a8a3239573c049862db86ea22ed8569dc53e042
hugeletters <ckuka71@gmail.com>
2024-05-08 01:58:37 +0200

    add todos

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,6 +2,11 @@ import type { NextPageWithLayout } from "@/layout";
 import { Layout } from "@/layout";
 import Head from "next/head";
 
+// todo - loading indicator
+// todo - how to use scanner
+// todo - made by
+// ? todo - offline indicator
+
 const Page: NextPageWithLayout = () => <div>about</div>;
 
 Page.getLayout = (children) => (
