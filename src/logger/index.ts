c9438584f1ff026c93bb35de15dbe8af310e135f
hugeletters <ckuka71@gmail.com>
2024-05-28 23:21:00 +0200

    client logger

---
+++ src/logger/index.ts
@@ -0,0 +1,19 @@
+import { FAILED_TO_FETCH_MESSAGE, getErrorMessage } from "@/error";
+import { trpcLazy } from "@/trpc";
+
+export const logger = {
+  error(error: unknown) {
+    console.error(error);
+    if (process.env.NODE_ENV !== "production") return;
+
+    const message = getErrorMessage(error);
+    if (message === FAILED_TO_FETCH_MESSAGE) return;
+
+    trpcLazy.client.logs.error.reportClientError
+      .mutate({
+        message,
+        stack: error instanceof Error ? error.stack : undefined,
+      })
+      .catch(console.error);
+  },
+};
