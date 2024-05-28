1ea15377b10e857c074bc8d9cd8a4e93a9a3c16d
hugeletters <ckuka71@gmail.com>
2024-05-28 23:54:45 +0200

    add error log formatter

--- src/server/api/logs/error.ts
+++ src/server/api/logs/error.ts
@@ -8,6 +8,7 @@ const clientErrorSchema = z.object({ message: z.string(), stack: z.string().opti
 const reportClientError = protectedProcedure
   .input(clientErrorSchema)
   .mutation(({ input, ctx: { session } }) => {
+    // todo - logs should rotate, no more than 500 messages
     return db
       .insert(log)
       .values({ log: createErrorLog(input), type: "error", user: session.user.id })
@@ -15,11 +16,28 @@ const reportClientError = protectedProcedure
   });
 
 type ClientError = z.infer<typeof clientErrorSchema>;
-function createErrorLog({ message, stack }: ClientError) {
-  const log = message + stack;
+function createErrorLog(error: ClientError) {
+  const log = formatErrorLog(error);
 
   if (log.length <= logMaxLength) return log;
   return log.slice(0, logMaxLength);
+}
+
+function formatErrorLog({ message, stack }: ClientError) {
+  if (!stack) return message;
+  const formattedStack = formatStack(stack);
+  if (!formattedStack) return message;
+
+  return message + "\n" + formattedStack;
+}
+
+function formatStack(stackString: string) {
+  const [header, ...stack] = stackString.split("\n");
+  if (!header) return null;
+
+  const formattedHeader = header.replace(/:.+$/, "");
+  const formattedStack = stack.map((stack) => stack.replace(/^\s*at /, "")).join("->");
+  return `${formattedHeader}:${formattedStack}`;
 }
 
 export const errorLogsRouter = createTRPCRouter({
