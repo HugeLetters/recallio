30a6de259f74913e393b2bf406a4614a28987976
hugeletters <ckuka71@gmail.com>
2024-05-29 00:09:04 +0200

    create cron handler helper

--- src/pages/api/file-cleanup.ts
+++ src/pages/api/file-cleanup.ts
@@ -1,22 +1,19 @@
+import { createCronHandler } from "@/server/cron";
 import { db } from "@/server/database/client";
 import { fileDeleteQueue } from "@/server/database/schema/file";
-import { env } from "@/server/env/index.mjs";
 import { throwExpectedError } from "@/server/error/trpc";
 import { utapi } from "@/server/uploadthing/api";
 import { ignore } from "@/utils";
-import { verifySignature } from "@upstash/qstash/dist/nextjs";
 import { lte } from "drizzle-orm";
-import type { NextApiRequest, NextApiResponse } from "next";
 
 export const config = { api: { bodyParser: false } };
-
-function innerHandler(_: NextApiRequest, res: NextApiResponse) {
+export default createCronHandler((_, res) => {
   return deletePendingFiles()
     .then(() => {
       res.status(200).json(null);
     })
     .catch(throwExpectedError(undefined));
-}
+});
 
 function deletePendingFiles() {
   return db.transaction(async (tx) => {
@@ -33,13 +30,3 @@ function deletePendingFiles() {
     return await utapi.deleteFiles(pendingFiles.map((file) => file.key)).then(ignore);
   });
 }
-
-const handler =
-  env.NEXT_PUBLIC_NODE_ENV !== "development"
-    ? verifySignature(innerHandler, {
-        currentSigningKey: env.QSTASH_CURRENT_SIGNING_KEY,
-        nextSigningKey: env.QSTASH_NEXT_SIGNING_KEY,
-      })
-    : innerHandler;
-
-export default handler;
