a380c361560b3565a84c4263996fb5e8b42c457e
hugeletters <ckuka71@gmail.com>
2024-05-28 23:51:52 +0200

    update logs schema

--- src/server/database/schema/logs.ts
+++ src/server/database/schema/logs.ts
@@ -1,13 +1,18 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { index, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { user, userIdLength } from "./user";
 import { timestampColumn } from "./utils";
 
 export const logMaxLength = 512;
-export const log = sqliteTable("log", {
-  log: text("log", { length: logMaxLength }).notNull(),
-  type: text("type", { length: 15 }).notNull().$type<"error">(),
-  user: text("user_id", { length: userIdLength })
-    .notNull()
-    .references(() => user.id),
-  createdAt: timestampColumn("created_at").notNull(),
-});
+export const log = sqliteTable(
+  "log",
+  {
+    log: text("log", { length: logMaxLength }).notNull(),
+    type: text("type", { length: 15 }).notNull().$type<"error">(),
+    user: text("user_id", { length: userIdLength }).references(() => user.id, {
+      onDelete: "set null",
+      onUpdate: "cascade",
+    }),
+    createdAt: timestampColumn("created_at").notNull(),
+  },
+  (table) => ({ createdAtIndex: index("log_created_at_index").on(table.createdAt) }),
+);
