91675e5f7e0d7c0baa375f02a13d68b86ee40e23
hugeletters <ckuka71@gmail.com>
2024-05-28 23:51:33 +0200

    update drizzle kit migrations to new v

--- scripts/database/migration/generate.ts
+++ scripts/database/migration/generate.ts
@@ -1,5 +1,5 @@
 import { execAsync } from "scripts/utils";
 
 export default function main() {
-  return execAsync("pnpm drizzle-kit generate:sqlite").then(console.log).catch(console.error);
+  return execAsync("pnpm drizzle-kit generate").then(console.log).catch(console.error);
 }
