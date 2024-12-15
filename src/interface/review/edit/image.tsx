e91deaddc0a889fd77b02b045fc5454e950cf73c
hugeletters <ckuka71@gmail.com>
2024-12-15 22:49:42 +0100

    add review image hook

--- src/interface/review/edit/image.tsx
+++ src/interface/review/edit/image.tsx
@@ -1,13 +1,16 @@
-import { useBlobUrl } from "@/image/blob";
+import { blobToFile, useBlobUrl } from "@/image/blob";
 import { ImagePickerButton } from "@/image/image-picker";
+import type { CropDimensions } from "@/image/utils";
+import { crop as cropImage } from "@/image/utils";
 import { toast } from "@/interface/toast";
 import { ImagePreview } from "@/product/components";
 import type { Model } from "@/state/type";
 import { tw } from "@/styles/tw";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import ResetIcon from "~icons/custom/reset";
 import DeleteIcon from "~icons/fluent-emoji-high-contrast/cross-mark";
-
-// todo - allow to crop already saved images
+import type { ReviewForm } from ".";
 
 export enum ImageAction {
   KEEP = "KEEP",
@@ -69,4 +72,140 @@ export function AttachedImage({ savedImage, value, setValue }: AttachedImageProp
       </ImagePickerButton>
     </div>
   );
+}
+
+// todo - cleanup
+export function useReviewImage(src: ReviewForm["image"]) {
+  const [rawValue, setRawValue] = useState<ImageState>(ImageAction.KEEP);
+  const [cropArea, setCropArea] = useState<CropDimensions | null>(null);
+
+  useEffect(() => {
+    reset();
+  }, [src]);
+
+  const { data: rawImageQueryData } = useAsyncState({
+    domain: "review_image",
+    dependencies: src,
+    queryFn(src) {
+      if (src === null) {
+        return null;
+      }
+
+      return fetch(src)
+        .then((res) => res.blob())
+        .then((blob) => blobToFile(blob, src));
+    },
+  });
+  function getRawImage(): File | null {
+    switch (rawValue) {
+      case ImageAction.KEEP:
+        return rawImageQueryData ?? null;
+      case ImageAction.DELETE:
+        return null;
+      default:
+        return rawValue satisfies File;
+    }
+  }
+  const rawImage = getRawImage();
+
+  const imageQuery = useAsyncState({
+    domain: "modified_review_image",
+    dependencies: { image: rawImage, crop: cropArea },
+    async queryFn({ image, crop }) {
+      if (!image) {
+        return null;
+      }
+
+      let output = image;
+
+      if (crop) {
+        const source = output;
+        output = await cropImage({
+          image: source,
+          width: crop.width,
+          height: crop.height,
+          left: crop.left,
+          top: crop.top,
+        }).then((blob) => blobToFile(blob, source.name));
+      }
+
+      return output;
+    },
+  });
+  const image: File | null = imageQuery.data ?? rawImage;
+
+  function getValue(): ImageState {
+    switch (rawValue) {
+      case ImageAction.KEEP:
+      case ImageAction.DELETE:
+        return rawValue;
+      default: {
+        rawValue satisfies File;
+        return image ?? ImageAction.KEEP;
+      }
+    }
+  }
+  const state = getValue();
+
+  function reset() {
+    setRawValue(ImageAction.KEEP);
+    setCropArea(null);
+  }
+
+  return {
+    state,
+    rawImage,
+    image,
+    setImage(this: void, image: File) {
+      setRawValue(image);
+      setCropArea(null);
+    },
+    delete(this: void) {
+      setRawValue(ImageAction.DELETE);
+      setCropArea(null);
+    },
+    reset,
+    effects: {
+      crop: {
+        value: cropArea,
+        set(this: void, area: typeof cropArea) {
+          setCropArea(area);
+
+          if (area === null || rawImage === null) {
+            return;
+          }
+          setRawValue(rawImage);
+        },
+      },
+    },
+  };
+}
+
+type AsyncStateOptions<TDeps, TOutput> = {
+  domain: string;
+  dependencies: TDeps;
+  queryFn: (input: TDeps) => TOutput | Promise<TOutput>;
+};
+
+function useAsyncState<TInput, TOutput>({
+  domain,
+  dependencies,
+  queryFn,
+}: AsyncStateOptions<TInput, TOutput>) {
+  const key = ["__client", domain, dependencies] as const;
+  const client = useQueryClient();
+
+  return useQuery({
+    queryKey: key,
+    queryFn({ queryKey: [_, __, deps] }) {
+      client.removeQueries({ exact: true, queryKey: key });
+      return queryFn(deps);
+    },
+    staleTime: Infinity,
+    networkMode: "always",
+    structuralSharing: false,
+    keepPreviousData: true,
+    retry: 3,
+    retryDelay: 0,
+  });
 }
