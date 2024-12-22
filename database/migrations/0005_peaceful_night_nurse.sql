e7cfcb3dffdea6e6c19fb7540896403b3a43825f
hugeletters <ckuka71@gmail.com>
2024-12-22 13:20:44 +0100

    add new trigger migration

---
+++ database/migrations/0005_peaceful_night_nurse.sql
@@ -0,0 +1,24 @@
+DROP TRIGGER IF EXISTS "update_product_meta_on_delete_review";--> statement-breakpoint
+DROP TRIGGER IF EXISTS "update_product_meta_on_new_review";--> statement-breakpoint
+DROP TRIGGER IF EXISTS "update_product_meta_on_update_review";--> statement-breakpoint
+DROP TRIGGER IF EXISTS "update_product_meta_on_delete_review";--> statement-breakpoint
+CREATE TRIGGER "update_product_meta_on_delete_review"
+	AFTER DELETE ON "review"
+	FOR EACH ROW WHEN "old"."is_private" = 0
+BEGIN
+	update "product_meta" set "public_review_count" = MAX(0, "product_meta"."public_review_count" - 1), "public_total_rating" = MAX(0, "product_meta"."public_total_rating" - "old"."rating") where "product_meta"."barcode" = "old"."barcode";
+END;--> statement-breakpoint
+DROP TRIGGER IF EXISTS "update_product_meta_on_new_review";--> statement-breakpoint
+CREATE TRIGGER "update_product_meta_on_new_review"
+	AFTER INSERT ON "review"
+	FOR EACH ROW WHEN "new"."is_private" = 0
+BEGIN
+	insert into "product_meta" ("barcode", "public_review_count", "public_total_rating") values ("new"."barcode", 1, "new"."rating") on conflict ("product_meta"."barcode") do update set "public_review_count" = "product_meta"."public_review_count" + 1, "public_total_rating" = "product_meta"."public_total_rating" + "excluded"."public_total_rating";
+END;--> statement-breakpoint
+DROP TRIGGER IF EXISTS "update_product_meta_on_update_review";--> statement-breakpoint
+CREATE TRIGGER "update_product_meta_on_update_review"
+	AFTER UPDATE OF "is_private" ON "review"
+	FOR EACH ROW WHEN "new"."is_private" <> "old"."is_private"
+BEGIN
+	insert into "product_meta" ("barcode", "public_review_count", "public_total_rating") values ("new"."barcode", CASE WHEN "new"."is_private" = 0 THEN 1 ELSE 0 END, CASE WHEN "new"."is_private" = 0 THEN "new"."rating" ELSE 0 END) on conflict ("product_meta"."barcode") do update set "public_review_count" = CASE WHEN "new"."is_private" = 0 THEN "product_meta"."public_review_count" + 1 ELSE MAX(0, "product_meta"."public_review_count" - 1) END, "public_total_rating" = CASE WHEN "new"."is_private" = 0 THEN "product_meta"."public_total_rating" + "new"."rating" ELSE MAX(0, "product_meta"."public_total_rating" - "new"."rating") END;
+END;
